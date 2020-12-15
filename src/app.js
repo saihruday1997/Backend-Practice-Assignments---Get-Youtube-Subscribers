04c7e85e5c4b85dd6c93c03f54bd5861eee940b8
saihruday1997 <saihruday.08@gmail.com>
2020-12-16 02:08:09 +0530

    chngs

--- src/app.js
+++ src/app.js
@@ -10,6 +10,15 @@ app.get("/subscribers" , (req , res) => {
     Subscriber.find().then((sub) => res.send(sub));
 });
 
+app.get("/subscribers/names" , (req , res) => {
+
+});
+
+app.get("/subscribers/:id", (req , res) => {
+    Subscriber.find({_id : req.params.id})
+        .then((sub) => res.send(sub))
+        .catch((err) => res.status(400).send({message: err.message}));
+});
 
 
 
