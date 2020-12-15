0051c095e32cb2cd1bcfae9f95d5deecab12595b
saihruday1997 <saihruday.08@gmail.com>
2020-12-16 00:08:01 +0530

    chngs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 
 const express = require('express');
-const app = express()
+const app = express();
+const data = require("./data");
 
 
 // Your code goes here
+
+app.get("/subscribers" , (req , res) => {
+    res.json(data);
+});
 
 
 
