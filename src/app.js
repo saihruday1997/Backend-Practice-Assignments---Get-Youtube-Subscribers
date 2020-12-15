5dd7460fe4a5a49a00f229bfbe30086ccec473c5
saihruday1997 <saihruday.08@gmail.com>
2020-12-16 01:48:48 +0530

    chngs

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 
 const express = require('express');
 const app = express();
-const data = require("./data");
+const Subscriber = require("./models/subscribers");
 
 
 // Your code goes here
 
-let Subscriber = mongoose.model("subscriber" , data);
-
 app.get("/subscribers" , (req , res) => {
-    subscriber.find().then((subscriber) => res.send(subscriber));
+    Subscriber.find().then((sub) => res.send(sub));
 });
 
 
